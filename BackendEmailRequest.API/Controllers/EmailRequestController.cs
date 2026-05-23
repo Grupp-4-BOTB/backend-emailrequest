823249f3b5630573aa2cab473a162afc2679a8f9
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-05-23 18:56:33 +0200

    Finished the controller, interface, dto, entity for the email that is sent out to the person being invited to join group. In the email that the user receive they will be able to go to another page on our website and fill in information and then be added to the group. however the page is not working yet, and it will be for module number 2. Also have not yet implemented the api to contact Gabriels database. ps. the reason i have added another page is due to the person being invited needs to fill in their ROLE as student or teacher.

---
+++ BackendEmailRequest.API/Controllers/EmailRequestController.cs
@@ -0,0 +1,42 @@
+﻿using BackendEmailRequest.API.DTOs;
+using BackendEmailRequest.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BackendEmailRequest.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmailRequestController : ControllerBase
+{
+    private readonly IEmailRequestService _emailService;
+
+    public EmailRequestController(IEmailRequestService emailService)
+    {
+        _emailService = emailService;
+    }
+
+
+
+
+
+    // EMAIL INVITE
+    // HANTERAR UTSÄNDNING AV EMAIL INBJUDAN TILL GRUPPEN
+
+    [HttpPost("emailinvite")]
+    public async Task<IActionResult> SendInvite([FromBody] InviteRequestDTO request) // inviteRequestDTO innehåller toEmail
+    {
+        // 1. HAR HÅRDKODAT MAILEN SOM ANVÄNDAREN FÅR I MAILET SÅLÄNGE! (Byta ut mot att den som skickade inbjudan sen ska stå här.)
+        string inviterEmail = "SAMUEL JOHANSSON";
+
+        // 2. VERCEL-länken som kopplar användarens email med sidan så sidan vet vem som går in
+        string inviteLink = $"https://lms-shiko.vercel.app/emailverification?email={request.RecipientEmail}";
+
+        // 3. Skicka till din service
+        await _emailService.SendInviteEmailAsync(request.RecipientEmail, inviterEmail, inviteLink);
+
+        return Ok(new { message = "The invite has been sent to email!" }); //skrivs ut när mailet skickats iväg på frontend
+    }
+}
+
+
