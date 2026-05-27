e02d4074c8b3872d1948e19ba4e283c764f0d692
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-05-27 16:15:35 +0200

    updates.

--- BackendEmailRequest.API/DTOs/InviteRequestDTO.cs
+++ BackendEmailRequest.API/DTOs/InviteRequestDTO.cs
@@ -4,8 +4,8 @@ public class InviteRequestDTO
 {
 
     public string RecipientEmail { get; set; } = string.Empty; //mottagaren
-    public string InviterEmail { get; set; } //Perosnen som BJUDER IN ska också vara i samma grupp
-    public int GroupId { get; set; } //Gruppen som man bjuds in till
+    public string InviterEmail { get; set; } = string.Empty; //Perosnen som BJUDER IN ska också vara i samma grupp
+    public int GroupId { get; set; } = 0; //Gruppen som man bjuds in till
 
 
     // DTON TRANSPORTERAR DET ANVÄNDAREN SKRIVER IN I PLACEHOLDERN, samt avsändaren och gruppen I FRONTEND OCH SKICKAR DET HIT TILL BACKEND
